646beb901d2d95321f1448d446ffb748334c69a5
Raden Hanif Satria Adjistama <rhanifsat@gmail.com>
2020-12-07 03:37:26 +0700

    Chapter 8

--- Chapter 8 Phython Project No. 13.py
+++ Chapter 8 Phython Project No. 13.py
@@ -4,4 +4,14 @@ nilaiMhs = [{'nim' : 'A01', 'nama' : 'Amir', 'mid' : 50, 'uas' : 80},
             {'nim' : 'A04', 'nama' : 'Dedi', 'mid' : 20, 'uas' : 30}, 
 	        {'nim' : 'A05', 'nama' : 'Fifi', 'mid' : 70, 'uas' : 40}]
 
-def nilaiakhir():
+def nilaitinggi(lst):
+    i = 0
+    lstakhir = []
+    for nilai in lst:
+        nilai = (round((lst[i]['mid'] + 2 * lst[i]['uas']) / 3,1), lst[i]['nim'], lst[i]['nama'])
+        i += 1
+        lstakhir.append(nilai)
+    lstakhir.sort(reverse = True)
+    print('Mahasiswa yang mendapatkan nilai tertinggi adalah', lstakhir[0][2], 'dengan nim', lstakhir[0][1], 'dengan perolehan nilai', lstakhir[0][0])
+
+nilaitinggi(nilaiMhs)
